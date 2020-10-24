02886af44d85d3df078dc746db5fcc56ce81f79c
Rashmildp <64850016+Rashmildp@users.noreply.github.com>
2020-10-24 14:44:03 +0530

    Create star.java

---
+++ star.java
@@ -0,0 +1,24 @@
+public class Star
+{ 
+    public static void Triangle(int n) 
+    { 
+        int i, j;  
+        for(i=0; i<n; i++)
+        {
+        for(j=2*(n-i); j>=0; j--) 
+            {           
+                System.out.print(" "); 
+            } 
+            for(j=0; j<=i; j++) 
+            {       
+                System.out.print("* "); 
+            }           
+            System.out.println(); 
+        } 
+    } 
+    public static void main(String args[]) 
+    { 
+        int n = 6; 
+        Triangle(n); 
+    } 
+}
